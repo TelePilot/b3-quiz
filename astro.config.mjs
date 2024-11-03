42895eae7e58cc177d664094cd5d6ba447c43f46
blixt <teodor.blixt@gmail.com>
2024-11-03 23:19:00 +0100

    clerk and dashboard

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@
 import { defineConfig, envField } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
-
+import node from '@astrojs/node'
 import react from '@astrojs/react'
+import clerk from '@clerk/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), clerk()],
   output: 'server',
+  adapter: node({ mode: 'standalone' }),
   experimental: {
     env: {
       schema: {
@@ -19,6 +21,14 @@ export default defineConfig({
         SUPA_KEY: envField.string({
           context: 'server',
           access: 'public',
+        }),
+        PUBLIC_CLERK_PUBLISHABLE_KEY: envField.string({
+          context: 'server',
+          access: 'public',
+        }),
+        CLERK_SECRET_KEY: envField.string({
+          context: 'server',
+          access: 'secret',
         }),
       },
     },
--- src/actions/client.ts
+++ src/actions/client.ts
@@ -1,4 +1,22 @@
 import { createClient } from '@supabase/supabase-js'
 import { SUPA_KEY, SUPA_URL } from 'astro:env/server'
+
 export const prerender = false
 export const supabase = createClient(SUPA_URL, SUPA_KEY)
+export const createClerkSupaClient = (clerkToken?: string) => {
+  return createClient(SUPA_URL, SUPA_KEY, {
+    global: {
+      // Get the custom Supabase token from Clerk
+      fetch: async (url, options = {}) => {
+        // Insert the Clerk Supabase token into the headers
+        const headers = new Headers(options?.headers)
+        headers.set('Authorization', `Bearer ${clerkToken}`)
+        // Now call the default fetch
+        return fetch(url, {
+          ...options,
+          headers,
+        })
+      },
+    },
+  })
+}
