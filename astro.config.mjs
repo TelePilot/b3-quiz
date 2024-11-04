9b9317cf7e79fe6567cfc223bc1f8456ccd8d9a9
blixt <teodor.blixt@gmail.com>
2024-11-04 18:10:31 +0100

    hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import clerk from '@clerk/astro'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), clerk()],
-  output: 'server',
+  output: 'hybrid',
   redirects: {
     dashboard: 'dashboard/new-question',
   },
